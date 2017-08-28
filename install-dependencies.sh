f0e239df823bde8423e3181e17aacf0fc21eaff3
ijm7 <ijmorton500@gmail.com>
2017-08-28 14:28:00 +0100

    Add correct build directory

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://github.com/google/gumbo-parser/archive/v0.10.1.tar.gz -O /tmp/gumbo-parser.tar.gz
 tar -xvf /tmp/gumbo-parser.tar.gz
-cd /tmp/gumbo-parser
+cd /tmp/gumbo-parser-0.10.1
 ./autogen.sh
 ./configure
 make
